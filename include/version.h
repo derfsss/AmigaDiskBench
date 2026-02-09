4730362a5b1c5e1cf9bbc2b9bdb598c6544f4895
derfsss <rich_gibbs@hotmail.com>
2026-02-09 20:15:33 +0000

    Update version strings to v1.9.14

--- include/version.h
+++ include/version.h
@@ -3,16 +3,16 @@
 
 #define APP_VERSION 1
 #define APP_REVISION 9
-#define APP_PATCH 12
-#define APP_VERSION_STR "1.9.12"
+#define APP_PATCH 14
+#define APP_VERSION_STR "1.9.14"
 #define APP_DATE "09.02.2026"
 #define APP_TITLE "AmigaDiskBench"
-#define APP_VER_TITLE "AmigaDiskBench v1.9.12"
-#define APP_DESCRIPTION "Amiga Disk Benchmark Utility v1.9.12"
+#define APP_VER_TITLE "AmigaDiskBench v1.9.14"
+#define APP_DESCRIPTION "Amiga Disk Benchmark Utility v1.9.14"
 #define APP_COPYRIGHT "(C) 2026 Antigravity Team"
-#define APP_ABOUT_MSG "AmigaDiskBench v1.9.12\n(C) 2026 Antigravity Team\nA modern benchmark for AmigaOS 4.x"
+#define APP_ABOUT_MSG "AmigaDiskBench v1.9.14\n(C) 2026 Antigravity Team\nA modern benchmark for AmigaOS 4.x"
 
 /* Amiga style $VER string */
-#define VER_STRING "$VER: AmigaDiskBench 1.9.12 (09.02.2026)\r\n"
+#define VER_STRING "$VER: AmigaDiskBench 1.9.14 (09.02.2026)\r\n"
 
 #endif /* VERSION_H */
