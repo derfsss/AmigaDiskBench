952d3033cbb3ad9853d46043b98f7681f1bae024
derfsss <rich_gibbs@hotmail.com>
2026-02-10 20:04:16 +0000

    Bump version to 2.0.2

--- include/version.h
+++ include/version.h
@@ -3,16 +3,16 @@
 
 #define APP_VERSION 2
 #define APP_REVISION 0
-#define APP_PATCH 1
-#define APP_VERSION_STR "2.0.1"
-#define APP_DATE "09.02.2026"
+#define APP_PATCH 2
+#define APP_VERSION_STR "2.0.2"
+#define APP_DATE "10.02.2026"
 #define APP_TITLE "AmigaDiskBench"
-#define APP_VER_TITLE "AmigaDiskBench v2.0.1"
-#define APP_DESCRIPTION "Amiga Disk Benchmark Utility v2.0.1"
+#define APP_VER_TITLE "AmigaDiskBench v2.0.2"
+#define APP_DESCRIPTION "Amiga Disk Benchmark Utility v2.0.2"
 #define APP_COPYRIGHT "(C) 2026 Antigravity Team"
-#define APP_ABOUT_MSG "AmigaDiskBench v2.0.1\n(C) 2026 Antigravity Team\nA modern benchmark for AmigaOS 4.x"
+#define APP_ABOUT_MSG "AmigaDiskBench v2.0.2\n(C) 2026 Antigravity Team\nA modern benchmark for AmigaOS 4.x"
 
 /* Amiga style $VER string */
-#define VER_STRING "$VER: AmigaDiskBench 2.0.1 (09.02.2026)\r\n"
+#define VER_STRING "$VER: AmigaDiskBench 2.0.2 (10.02.2026)\r\n"
 
 #endif /* VERSION_H */
