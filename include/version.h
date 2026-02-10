21a6edbe54d86dd045d7773daba62a4b60e8f38d
derfsss <rich_gibbs@hotmail.com>
2026-02-10 20:06:46 +0000

    Refactor version.h: use APP_VERSION_STR and APP_DATE for consistency

--- include/version.h
+++ include/version.h
@@ -7,12 +7,12 @@
 #define APP_VERSION_STR "2.0.2"
 #define APP_DATE "10.02.2026"
 #define APP_TITLE "AmigaDiskBench"
-#define APP_VER_TITLE "AmigaDiskBench v2.0.2"
-#define APP_DESCRIPTION "Amiga Disk Benchmark Utility v2.0.2"
+#define APP_VER_TITLE APP_TITLE " v" APP_VERSION_STR
+#define APP_DESCRIPTION "Amiga Disk Benchmark Utility v" APP_VERSION_STR
 #define APP_COPYRIGHT "(C) 2026 Antigravity Team"
-#define APP_ABOUT_MSG "AmigaDiskBench v2.0.2\n(C) 2026 Antigravity Team\nA modern benchmark for AmigaOS 4.x"
+#define APP_ABOUT_MSG APP_TITLE " v" APP_VERSION_STR "\n" APP_COPYRIGHT "\nA modern benchmark for AmigaOS 4.x"
 
 /* Amiga style $VER string */
-#define VER_STRING "$VER: AmigaDiskBench 2.0.2 (10.02.2026)\r\n"
+#define VER_STRING "$VER: " APP_TITLE " " APP_VERSION_STR " (" APP_DATE ")\r\n"
 
 #endif /* VERSION_H */
